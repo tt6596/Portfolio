606387a11ecf7b2a7816add83d5fa5e472d4ce56
tt6596 <145344962+tt6596@users.noreply.github.com>
2026-02-02 12:46:07 +0800

    Delete 9B132042期末 directory

--- 9B132042期末/CreateuserTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[userTable] (
-    [sid]      NVARCHAR (50) NOT NULL,
-    [name]     NVARCHAR (50) NULL,
-    [password] NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([sid] ASC)
-);
-Go
-INSERT INTO [dbo].[userTable] ([sid], [name], [password]) VALUES (N'9A632100', N'李樹', N'12345')
-GO
